993858a063174330b51d51ac65402f939132bd5f
Tomas Aranda <tomasaranda2018@gmail.com>
2022-06-23 12:41:28 -0300

    add function login

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,9 +1,11 @@
 import express from 'express';
-import {registerSignUp} from '../controllers/userControllers.js'
+import {registerSignUp, loginUser} from '../controllers/userControllers.js'
 
 const router = express.Router()
 
 router.post('/sign-up', registerSignUp )
+
+router.post('/login', loginUser )
 
 
 export default router
