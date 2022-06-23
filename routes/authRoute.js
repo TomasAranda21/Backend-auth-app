0cd8ec4e5c51a22e95b43a7aa0d319104d5e5a9f
Tomas Aranda <tomasaranda2018@gmail.com>
2022-06-23 13:25:15 -0300

    add to profile route

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,11 +1,21 @@
 import express from 'express';
-import {registerSignUp, loginUser} from '../controllers/userControllers.js'
+import {registerSignUp, loginUser, profileUser} from '../controllers/userControllers.js'
+import checkAuth from '../middleware/authMiddleware.js';
 
 const router = express.Router()
 
 router.post('/sign-up', registerSignUp )
 
 router.post('/login', loginUser )
+
+
+
+// Private Area
+
+router.get('/profile', checkAuth, profileUser)
+
+
+
 
 
 export default router
