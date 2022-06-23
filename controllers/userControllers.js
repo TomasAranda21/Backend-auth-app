ed682a713849f53546e94a92bf3bfa5d353dec7e
Tomas Aranda <tomasaranda2018@gmail.com>
2022-06-23 12:55:18 -0300

    add function jwt for login and login function

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,3 +1,4 @@
+import createJWT from '../helpers/createJWT.js'
 import usersModel from '../models/usersModel.js'
 
 
@@ -10,7 +11,7 @@ const registerSignUp = async (req, res) => {
 
     if(userExists) {
 
-        const error = new Error("Usuario ya registrado")
+        const error = new Error("the email already exists")
 
         return res.status(400).json({msg: error.message}) 
     }
@@ -39,6 +40,30 @@ const registerSignUp = async (req, res) => {
 
 const loginUser = async(req, res) => {
 
+    const { email, password } = req.body
+
+    const user = await usersModel.findOne({ email})
+
+    if(!user){
+        const error = new Error("the email dont exist")
+
+        return res.status(400).json({msg: error.message}) 
+    }
+
+    if(await user.checkPassword(password)){
+
+        res.json({
+            name: user.name,
+            email: user.email,
+            token: createJWT(user._id)
+        })
+
+    }
+    else{
+        const error = new Error("La contraseña o el Usuario es incorrecto")
+
+        return res.status(403).json({msg: error.message})
+    }
 
 }
 
